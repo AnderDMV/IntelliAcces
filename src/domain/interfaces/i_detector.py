46258fd898762fbb86e810f40b28af5f0367a4e9
AnderDMV <106932806+AnderDMV@users.noreply.github.com>
2026-07-12 23:12:11 -0500

    Changes docstrings

--- src/domain/interfaces/i_detector.py
+++ src/domain/interfaces/i_detector.py
@@ -3,13 +3,15 @@ import numpy as np
 
 class IDetector(ABC):
     """
-        Interface for blink calcs
+        Interface for detectors class type
+    attributes:
+    initialized (bool) bool to initialize general configuration once
     """
     initialized = False
     
     @abstractmethod
     def detect(self, frame: np.ndarray):
         """
-        Processes the frame by implementing facial detection AI and saving the processed frame.
+        Processes the frame by implementing facial detection AI and configuration, saving the processed frame with the detections (face landmarks or others).
         """
         self.frame_processed =  frame        
